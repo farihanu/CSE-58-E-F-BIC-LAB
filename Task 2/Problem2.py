3924d5c4776611b5fa9330c0b29fbbb845c993f2
farihanu <farihamu05@gmail.com>
2026-06-09 15:58:00 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,18 @@
 
+text=input("Enter DNA String: ")
+k= int(input("Enter k:" ))
+
+count={}
+
+for i in range(len(text)-k+1):
+    pattern=text[i:i+k]
+    if pattern in count:
+        count[pattern]+=1
+    else:
+        count[pattern]=1
+        
+max_count=max(count.values())
+        
+for pattern in count:
+            if count[pattern]==max_count:
+                print(pattern,end=" ")
