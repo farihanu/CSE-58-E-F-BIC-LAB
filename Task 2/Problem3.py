722d33c445ac6db9ce0193c0fa6e0000058caa0f
farihanu <farihamu05@gmail.com>
2026-06-09 15:59:08 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
 
+pattern=input()
+complement={
+    "A":"T",
+    "T":"A",
+    "G":"C",
+    "C":"G"
+}
+
+reverse_complement =""
+for nucleotide in pattern:
+    reverse_complement += complement[nucleotide]
+    
+reverse_complement=reverse_complement[::-1]
+print(reverse_complement)
