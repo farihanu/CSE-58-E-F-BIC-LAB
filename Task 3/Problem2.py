eeaba265a93258d9a71b4c6a0b4fcddc65fd6b6d
farihanu <farihamu05@gmail.com>
2026-06-16 16:00:18 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,18 @@
 
+pattern=input("enter pattern:")
+text=input("enter text:")
+d=int(input("enter d:"))
+
+count1=0
+for i in range(len(text)-len(pattern)+1):
+    st=text[i:i+len(pattern)]
+    count=0
+    for j in range(len(pattern)):
+        if st[j]!=pattern[j]:
+            count+=1
+    if count<=d:
+      print(i," ",end="")
+
+
+
+
