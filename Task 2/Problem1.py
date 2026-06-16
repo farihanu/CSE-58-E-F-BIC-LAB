78eb9a1eaedf68689f608fa860448560ff2fefd5
farihanu <farihamu05@gmail.com>
2026-06-16 15:37:13 +0600

    Update Problem1.py

--- Task 2/Problem1.py
+++ Task 2/Problem1.py
@@ -1,1 +1,8 @@
-
+text=input("Enter text: ")
+pattern=input("Enter pattern: ")
+count=0
+for i in range(len(text)-len(pattern)+1):
+    str=text[i:i+len(pattern)]
+    if str==pattern:
+        count+=1
+print(count)        
