f995f07d5b9d0059d624f6469a589a54de551f21
farihanu <farihamu05@gmail.com>
2026-06-16 14:08:39 +0600

    Implement pattern matching in Problem4.py
    Add pattern matching functionality to search for a given pattern in the input text.

--- Task 2/Problem4.py
+++ Task 2/Problem4.py
@@ -1,1 +1,8 @@
+pattern = input("Enter pattern: ")
+text= input("Enter text:")
+for i in range(len(text)-len(pattern)+1):
+    str = text[i:i+len(pattern)]
+    if str==pattern:
+        print(i," ",end="")
+    
 
